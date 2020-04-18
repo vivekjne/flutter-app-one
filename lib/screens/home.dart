c1c6739b5e30d17b4b14ce3e011d2629e7bb8e1f
vivekjoy <vivek.joy@litmus7.com>
2020-04-18 23:10:12 +0530

    added create screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_twitch/screens/home_tabs/feed.dart';
 import 'home_tabs/discover.dart';
+import 'home_tabs/create.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -15,10 +16,7 @@ class _HomeState extends State<Home> {
   final List<Widget> _widgetOptions = <Widget>[
     Feed(),
     Discover(),
-    Text(
-      'Index 2:Create',
-      style: optionsStyle,
-    ),
+    Create(),
     Text(
       'Index 3:Notifications',
       style: optionsStyle,
@@ -30,6 +28,9 @@ class _HomeState extends State<Home> {
   ];
 
   void _onItemTapped(int index) {
+    // if(index==2){
+    //   Navigator.push(context, MaterialPageRoute(builder: (BuildContext context)=>))
+    // }
     setState(() {
       selectedIndex = index;
     });
